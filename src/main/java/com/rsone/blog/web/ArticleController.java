ebde35d923cf075c03e317c9150f7581de34ddb3
aroussi <abdelghani.roussi@adservio.fr>
2018-04-22 22:56:27 +0200

    [improve] rmv unused import

--- src/main/java/com/rsone/blog/web/ArticleController.java
+++ src/main/java/com/rsone/blog/web/ArticleController.java
@@ -1,7 +1,5 @@
 package com.rsone.blog.web;
 
-import com.rsone.blog.model.Article;
-import com.rsone.blog.service.ArticleService;
 import com.rsone.blog.web.handler.ArticleHandlerFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-
-import java.util.stream.Collectors;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @RestController
 public class ArticleController {
